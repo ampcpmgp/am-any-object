8101d150b48768415bb6eae1afaa7e459f47d3f3
ampcpmgp <ampcpmgp@gmail.com>
2024-07-01 23:01:19 +0900

    ci: coverage settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
+
+    coverage: {
+      reporter: ["json-summary", "json"],
+      reportOnFailure: true,
+    },
   },
 });
