d68d050cb2d430426ded97e26c623b5aa19a2ae9
Akira Makino <ampcpmgp@gmail.com>
2025-06-13 10:14:36 +0900

    Update any-object.ts
    Signed-off-by: Akira Makino <ampcpmgp@gmail.com>

--- src/any-object.ts
+++ src/any-object.ts
@@ -1,6 +1,6 @@
 /**
  * Return error-free and type-safe shallow object
  */
-export function anyObject<T>(): T {
-  return {} as unknown as T;
+export function anyObject<T>(extended?: Partial<T>): T {
+  return { ...extended } as unknown as T;
 }
